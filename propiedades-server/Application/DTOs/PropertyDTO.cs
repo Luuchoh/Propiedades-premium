54ca8caa45b55dc8d0c2ef9eac13d84949771745
Luis Hernandez <luxo997@gmail.com>
2025-08-22 15:13:49 -0500

    Add: Create PropertyDTO and Property model classes for property management

---
+++ propiedades-server/Application/DTOs/PropertyDTO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class PropertyDTO
+    {
+        public string PropertyName { get; set; } = null!;
+
+        public string Address { get; set; } = null!;
+
+        public string Price { get; set; } = null!;
+
+        public string Rooms { get; set; } = null!;
+
+        public string Bathrooms { get; set; } = null!;
+
+        public string Area { get; set; } = null!;
+
+        public string YearConstruction { get; set; } = null!;
+
+        public string AnnualTax { get; set; } = null!;
+
+        public string MonthlyExpenses { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public string Features { get; set; } = null!;
+
+        public string Photo { get; set; } = null!;
+    }
+}
---
+++ propiedades-server/Domain/Models/Property.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Domain.Models
+{
+    public class Property
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? IdProperty { get; set; }
+
+        [BsonElement("Name")]
+        public string PropertyName { get; set; } = null!;
+
+        public string Address { get; set; } = null!;
+
+        public string Price { get; set; } = null!;
+
+        public string Rooms { get; set; } = null!;
+
+        public string Bathrooms { get; set; } = null!;
+
+        public string Area { get; set; } = null!;
+
+        public string YearConstruction { get; set; } = null!;
+
+        public string AnnualTax { get; set; } = null!;
+
+        public string MonthlyExpenses{ get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public string Features { get; set; } = null!;
+
+        public string Photo { get; set; } = null!;
+    }
+}
