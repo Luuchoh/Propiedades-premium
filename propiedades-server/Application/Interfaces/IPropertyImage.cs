b5c181f667c5099e238e5a97461514e13b22d7ae
Luis Hernandez <luxo997@gmail.com>
2025-08-23 10:36:30 -0500

    Add: Implement IPropertyImage interface and PropertyImageService for managing property images

---
+++ propiedades-server/Application/Interfaces/IPropertyImage.cs
@@ -0,0 +1,19 @@
+﻿using Application.DTOs;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IPropertyImage
+    {
+        Task<PropertyImage?> GetPropertyImageByIdAsync(string generalId);
+
+        Task<PropertyImage> CreateAsync(PropertyImageDTO propertyImageDTO);
+
+        Task UpdateAsync(PropertyImageDTO propertyImageDTO);
+    }
+}
---
+++ propiedades-server/Infraestructure/Persistence/Services/PropertyImageService.cs
@@ -0,0 +1,55 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Domain.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace Infraestructure.Persistence.Services
+{
+    public class PropertyImageService : IPropertyImage
+    {
+        private readonly IMongoCollection<PropertyImage> _images;
+
+        public PropertyImageService(
+            IOptions<PropertyPremiumDatabaseSettings> PropertyPremiumDatabaseSettings)
+        {
+            var mongoClient = new MongoClient(
+                PropertyPremiumDatabaseSettings.Value.ConnectionString);
+
+            var mongoDatabase = mongoClient.GetDatabase(
+                PropertyPremiumDatabaseSettings.Value.DatabaseName);
+            _images = mongoDatabase.GetCollection<PropertyImage>(
+                PropertyPremiumDatabaseSettings.Value.PropertyImageCollectionName);
+        }
+
+        public async Task<PropertyImage> GetPropertyImageByIdAsync(string generalId)
+        {
+            return await _images.Find(i => i.IdProperty == generalId).FirstOrDefaultAsync();
+        }
+
+        public async Task<PropertyImage> CreateAsync(PropertyImageDTO propertyImageDTO)
+        {
+            PropertyImage newImage = new ()
+            {
+                IdProperty = propertyImageDTO.IdProperty,
+                File = propertyImageDTO?.File,
+                Enable = true
+            };
+            await _images.InsertOneAsync(newImage);
+
+            return newImage;
+        }
+
+        public async Task UpdateAsync(PropertyImageDTO propertyImageDTO)
+        {
+            var updatePropertyImage = Builders<PropertyImage>.Update
+                .Set(p => p.IdProperty, propertyImageDTO.IdProperty)
+                .Set(p => p.File, propertyImageDTO.File)
+                .Set(p => p.Enable, propertyImageDTO.Enable);
+
+            await _images.UpdateOneAsync(x => x.IdPropertyImage == propertyImageDTO.IdPropertyImage, updatePropertyImage);
+
+        }
+
+    }
+}
