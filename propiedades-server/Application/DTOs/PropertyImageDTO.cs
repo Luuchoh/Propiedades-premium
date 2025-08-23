5d6933fb63698b312efdf696e104b5ac185cdda7
Luis Hernandez <luxo997@gmail.com>
2025-08-23 10:34:00 -0500

    Add: Create PropertyImageDTO and PropertyImage model classes for property image management

---
+++ propiedades-server/Application/DTOs/PropertyImageDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class PropertyImageDTO
+    {
+        public string? IdPropertyImage { get; set; }
+
+        public string? IdProperty { get; set; }
+
+        public string? File { get; set; }
+
+        public bool Enable { get; set; }
+    }
+}
